877a8a1a482d746e9f5ba730e8855e61b8ae3511
unknown <Louisevirtudazo@gmail.com>
2024-05-09 22:38:20 +0800

    Jobs listing and create

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,30 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import JobPage, {jobLoader} from "./pages/JobPage.jsx";
+import AddJobPage from "./pages/AddJobPage.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />}></Route>
+      <Route path="/jobs/create" element={<AddJobPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}></Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title="Test title" subtitle="This is subtitle" />
-      <HomeCards />
-      <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? "bg-black text-white hover:bg-gray-900 rounded-md px-3 py-2"
+      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
   return (
     <>
       <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -13,35 +18,28 @@ const Navbar = () => {
                 className="flex flex-shrink-0 items-center mr-4"
                 href="/index.html"
               >
-                <img
-                  className="h-10 w-auto"
-                  src={logo}
-                  alt="React Jobs"
-                />
+                <img className="h-10 w-auto" src={logo} alt="React Jobs" />
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   React Jobs
                 </span>
               </a>
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
-                  <a
-                    href="/index.html"
-                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  <NavLink
+                    to="/"
+                    className={linkClass}
                   >
                     Home
-                  </a>
-                  <a
-                    href="/jobs.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
+                  </NavLink>
+                  <NavLink to="/jobs" className={linkClass}>
                     Jobs
-                  </a>
-                  <a
-                    href="/add-job.html"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  </NavLink>
+                  <NavLink
+                    to="/jobs/create"
+                    className={linkClass}
                   >
                     Add Job
-                  </a>
+                  </NavLink>
                 </div>
               </div>
             </div>
